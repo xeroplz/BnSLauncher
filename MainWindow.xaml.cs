eab97ce782a826bd9d2af31dd3efedbee989510b
xeroplz <jtjonson16@gmail.com>
2021-03-30 21:31:26 -0400

    misc update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace BnSLauncher
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private string openedPanel = "";
+		private string instructions = "instructions";
+		private string options = "options";
+		private string acknowledgements = "acknowledgements";
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		// Hides instructions panel elements
+		private void removeInstructions()
+		{
+			
+		}
+
+		// Hides options panel elements
+		private void removeOptions()
+		{
+			Unattended_Checkbox.Visibility = Visibility.Hidden;
+			No_Texture_Stream.Visibility = Visibility.Hidden;
+			Use_All_Available_Cores.Visibility = Visibility.Hidden;
+			Use_Modded_XML.Visibility = Visibility.Hidden;
+		}
+
+		// Hides acknowledgements panel elements
+		private void removeAcknowledgements()
+		{
+			
+		}
+
+		// Hides other elements if one panel opens
+		private void checkState()
+		{
+			// Instructions Open
+			if (openedPanel == instructions)
+			{
+				// Hide Options / Acknowledgements
+				removeOptions();
+				removeAcknowledgements();
+			}
+			else if (openedPanel == options)
+			{
+				// Hide Instructions / Acknowledgements
+				removeInstructions();
+				removeAcknowledgements();
+			}
+			else if (openedPanel == acknowledgements)
+			{
+				// Hide Instructions / Options
+				removeInstructions();
+				removeOptions();
+			}
 		}
 
 		// On activation, reset window style
@@ -48,6 +98,27 @@ namespace BnSLauncher
 		private void Minimize_Button_MouseDown(object sender, MouseButtonEventArgs e)
 		{
 			WindowState = WindowState.Minimized;
+		}
+
+		// View instructions
+		private void Instructions_Button_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			openedPanel = "instructions";
+
+			checkState();
+		}
+
+		// View options/launch
+		private void Options_Launch_Button_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			openedPanel = "options";
+
+			Unattended_Checkbox.Visibility = Visibility.Visible;
+			No_Texture_Stream.Visibility = Visibility.Visible;
+			Use_All_Available_Cores.Visibility = Visibility.Visible;
+			Use_Modded_XML.Visibility = Visibility.Visible;
+
+			checkState();
 		}
 	}
 }
